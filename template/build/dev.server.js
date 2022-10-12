6b19d35b74797e0555f81677bcb47c59c9faa02b
caiyong <yong.cai@kapeixi.com>
2022-10-12 11:04:52 +0800

    ci

--- template/build/dev.server.js
+++ template/build/dev.server.js
@@ -1,16 +1,6 @@
-process.env.NODE_ENV = 'development'
-import WebpackDevServer from 'webpack-dev-server';
-import webpack from 'webpack';
-import webpackConfig from './webpack.dev.config';
-import openInEditor from 'launch-editor-middleware'
-const compiler = webpack(webpackConfig);
-
-const localServer = new WebpackDevServer({
-  onBeforeSetupMiddleware({app}) {
-    app?.use('/__open-in-editor', openInEditor('code'));
-  },
-}, compiler);
-
-localServer.start()
+require('@babel/register')({
+  extensions: ['.ts', '.js'],
+});
+require('./server');
 
 
