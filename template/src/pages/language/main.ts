141b2cc70cfaa8b46e561e4779b29017da681f93
caiyong <yong.cai@kapeixi.com>
2022-10-12 11:26:34 +0800

    ci

--- template/src/pages/language/main.ts
+++ template/src/pages/language/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 import App from './app.vue';
 import {initI18n, site} from '@/site';
+{{#if tailwind}}
 import 'tailwindcss/tailwind.css';
+{{/if}}
 
 console.log('站点配置', site)
 async function bootstrap () {
