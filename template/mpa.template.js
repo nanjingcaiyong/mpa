f1f0f9d44a0a26ac9971909e92084881e8f0432b
caiyong <yong.cai@kapeixi.com>
2022-11-17 15:13:17 +0800

    fix

--- template/mpa.template.js
+++ template/mpa.template.js
@@ -4,15 +4,15 @@ module.exports = {
       return Array.from(str).reduce((str, k, index) => str += (index === 0 ? k.toUpperCase() : k), '');
     };
     return {
-      'index.vue': 
+      'app.vue': 
 `<template>Hello world</template>
 <script>
 import { defineComponent } from 'vue';
 export default defineComponent({
-  name1: '${firstUppercase(pageName)}'
+  name: '${firstUppercase(pageName)}'
 });
 </script>`,
-      'index.ts': 
+      'main.{{#if ts}}t{{else}}js{/if}': 
 `import { createApp } from 'vue';
 import App from './app.vue';
 createApp(App).mount('#${firstUppercase(pageName)}');`
